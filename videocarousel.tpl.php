1fa81c2096440d452faa4d32a77373ad3bb9d2ea
Haza <meyer.nicolas@gmail.com>
2010-03-01 11:42:42 +0100

    some styling

--- videocarousel.tpl.php
+++ videocarousel.tpl.php
@@ -1,7 +1,11 @@
-<div class="videocarousel-container">
-  <?php  print $jq_container; ?>
-</div>
+<div class="videocarousel-main-container" style="width: 495px;" >
+  <div class="videocarousel-container" style="margin: 0 auto; text-align: center">
+    <?php  print $jq_container; ?>
+  </div>
 
-<ul id="<?php print $name; ?>" class="jcarousel-skin-<?php print $skin; ?>">
-  <?php print $jq_listing; ?>
-</ul>
+  <ul id="<?php print $name; ?>" class="jcarousel-skin-<?php print $skin; ?>">
+    <?php print $jq_listing; ?>
+  </ul>
+
+
+</div>
