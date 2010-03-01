97f2c6dba024c7c25a189acaf3673a1046cbde0b
Haza <meyer.nicolas@gmail.com>
2010-03-01 18:25:24 +0100

    display title and body, update view and some js stuff

--- videocarousel.js
+++ videocarousel.js
@@ -1,6 +1,10 @@
 Drupal.behaviors.videocarousel = function (context) {
   $(".videocarousel-item").click(function () {
     $(".videocarousel-video-container").hide();
+    $(".videocarousel-item-body").hide();
+    $(".videocarousel-item-title").hide();
     $("#"+this.id + "-main").show();
+    $("#"+this.id + "-title").show();
+    $("#"+this.id + "-body").show();
   });
 }; 
--- videocarousel.tpl.php
+++ videocarousel.tpl.php
@@ -2,8 +2,18 @@
   <div class="videocarousel-container" style="margin: 0 auto; text-align: center">
     <?php  print $jq_container; ?>
   </div>
-
+  <?php
+  /*
+   * @TODO : CLEAN !!!!!
+   *
+   */
+  ?>
+  <div style="height: 60px;">
+    <?php print $jq_body; ?>
+  </div>
   <ul id="<?php print $name; ?>" class="jcarousel-skin-<?php print $skin; ?>">
     <?php print $jq_listing; ?>
   </ul>
+
+  
 </div>
