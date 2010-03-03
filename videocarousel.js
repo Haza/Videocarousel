bf6a33e338654e972b7029ba397fb2d3d6f7d839
Haza <meyer.nicolas@gmail.com>
2010-03-03 18:54:18 +0100

    tweak js and css

--- videocarousel.js
+++ videocarousel.js
@@ -1,10 +1,13 @@
 Drupal.behaviors.videocarousel = function (context) {
+  $("#videocarousel-item-1").css("opacity", "0.5")
   $(".videocarousel-item").click(function () {
+    $(".videocarousel-item").css("opacity", "1")
     $(".videocarousel-video-container").hide();
     $(".videocarousel-item-body").hide();
     $(".videocarousel-item-title").hide();
     $("#"+this.id + "-main").show();
     $("#"+this.id + "-title").show();
     $("#"+this.id + "-body").show();
+    $("#"+this.id).css("opacity","0.5");
   });
 }; 
